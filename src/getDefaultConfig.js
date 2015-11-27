9823551c3dbd1eef62ee296d1f1537355833ebc9
vagusX <vaguxl@gmail.com>
2015-11-27 13:30:04 +0800

    fix loader query part

--- src/getDefaultConfig.js
+++ src/getDefaultConfig.js
@@ -11,12 +11,12 @@ let getCommonConfig = {
     const loaders = [
       { test: /\.js$/, loader: 'babel', query: babelQuery, exclude: /node_modules/ },
       { test: /\.json$/, loader: 'json' },
-      { test: /\.(png|jpg|gif)$/, loader: 'url?limit=2048', name: `images/${name}` },
-      { test: /\.woff$/, loader: 'url?limit=100&minetype=application/font-woff', name: `fonts/${name}` },
-      { test: /\.woff2$/, loader: 'url?limit=100&minetype=application/font-woff2', name: `fonts/${name}` },
-      { test: /\.ttf$/, loader: 'url?limit=100&minetype=application/octet-stream', name: `fonts/${name}` },
-      { test: /\.eot$/, loader: 'url?limit=100', name: `fonts/${name}` },
-      { test: /\.svg$/, loader: 'url?limit=10000&minetype=image/svg+xml', name: `fonts/${name}` }
+      { test: /\.(png|jpg|gif)$/, loader: 'url', query: { limit: 2048, name: `images/${name}` } },
+      { test: /\.woff$/, loader: 'url', query: { limit: 100, minetype: 'application/font-woff', name: `fonts/${name}` } },
+      { test: /\.woff2$/, loader: 'url', query: { limit: 100, minetype: 'application/font-woff2', name: `fonts/${name}` } },
+      { test: /\.ttf$/, loader: 'url', query: { limit: 100, minetype: 'application/octet-stream', name: `fonts/${name}` } },
+      { test: /\.eot$/, loader: 'url', query: { limit: 100, name: `fonts/${name}` } },
+      { test: /\.svg$/, loader: 'url', query: { limit: 10000, minetype: 'image/svg+xml', name: `fonts/${name}` } }
     ];
     return loaders.concat(this.getCssLoaders(args))
   },
