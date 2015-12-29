d3d8ebff22f3818d96ff851ad7be02640695620f
vagusX <vagusxl@gmail.com>
2015-12-29 16:22:07 +0800

    fix when vendor is empty

--- src/methods/getEntry.js
+++ src/methods/getEntry.js
@@ -2,8 +2,12 @@ export default ({ production, entry, vendor}) => {
   // hmr enable via env
   !production && vendor.unshift('webpack-hot-middleware/client');
 
-  return {
-    main: entry,
-    vendor: vendor
+  if (vendor.length) {
+    return {
+      main: entry,
+      vendor: vendor
+    }
+  } else {
+    return [entry]
   }
 }
--- src/methods/getPlugins.js
+++ src/methods/getPlugins.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-export default ({ production, index, isComponent }) => {
+export default ({ production, index, vendor, isComponent }) => {
   // accept a boolean depending on env is production or not
   const vendorJsName = production
     ? '[name].[chunkhash:8].js'
@@ -16,14 +16,19 @@ export default ({ production, index, isComponent }) => {
   let plugins = [
     new webpack.DefinePlugin({'process.env.NODE_ENV': env}),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: vendorJsName, minChunks: Infinity}),
     new webpack.NoErrorsPlugin()
   ];
+  if (vendor.length) {
+    plugins = [
+      ...plugins,
+      new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: vendorJsName, minChunks: Infinity}),
+    ];
+  }
   if (!isComponent || !production) {
     plugins = [
       ...plugins,
       new HtmlWebpackPlugin({ templateContent: index, minify: {collapseWhitespace: true}, inject: 'body' }),
-    ]
+    ];
   }
   if (production) {
     plugins = [
