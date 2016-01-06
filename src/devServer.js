d3b38f8bfeecea9f8087d20a2c1f8f0939a2578b
Louis <yuluyi1991@gmail.com>
2016-01-06 16:53:07 +0800

    fix url issue

--- src/devServer.js
+++ src/devServer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackDevMiddleware from './dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import { findAPortNotInUse } from 'portscanner';
 
