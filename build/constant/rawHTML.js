bf9e9ef1cf9d560def0697f07fa6b81b7f8b2d94
vagusX <vagusxl@gmail.com>
2016-01-06 11:54:01 +0800

    rm build folder

--- build/constant/rawHTML.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var rawHTML = "\n  <!DOCTYPE html>\n  <html>\n  <head>\n    <meta charset=\"UTF-8\">\n    <title>pkgName</title>\n  </head>\n  <body>\n    <div id=\"root\"></div>\n  </body>\n  </html>\n";
-
-exports.default = rawHTML;
