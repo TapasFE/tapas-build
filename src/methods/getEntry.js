f6161570517a99f665e684625fd68fcc3ddd037d
Louis <yuluyi1991@gmail.com>
2016-01-14 10:48:03 +0800

    changed the tapas-externals enable logic

--- src/methods/getEntry.js
+++ src/methods/getEntry.js
@@ -2,6 +2,9 @@ export default ({ production, entry, vendor, autoExternals, isComponent}) => {
   // hmr enable via env
   !production && vendor.unshift('webpack-hot-middleware/client');
 
+  // 如果autoExternals 为 true， 用tapas-externals 作为amd loader
+  autoExternals && vendor.unshift('tapas-externals');
+
   // async/await 的runtime
   !isComponent && vendor.unshift('babel-polyfill');
 
