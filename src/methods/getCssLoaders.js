bea771662fa9719ed30fab7c6e5020b7542f7e3d
Louis <yuluyi1991@gmail.com>
2016-04-24 21:08:43 +0800

    feat: add sass loader

--- src/methods/getCssLoaders.js
+++ src/methods/getCssLoaders.js
@@ -6,16 +6,22 @@ export default ({ extractCss, cssModules }) => {
   let cssLoaderGlobal = 'css!postcss?pack=default';
   let lessLoaderLocal = 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss?pack=default!less';
   let lessLoaderGlobal = 'css!postcss?pack=default!less';
+  let sassLoaderLocal = 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss?pack=sass';
+  let sassLoaderGlobal = 'css?importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss?pack=sass';
   if (extractCss) {
     cssLoaderLocal = ExtractTextPlugin.extract('style', cssLoaderLocal);
     cssLoaderGlobal = ExtractTextPlugin.extract('style', cssLoaderGlobal);
     lessLoaderLocal = ExtractTextPlugin.extract('style', lessLoaderLocal);
     lessLoaderGlobal = ExtractTextPlugin.extract('style', lessLoaderGlobal);
+    sassLoaderLocal = ExtractTextPlugin.extract('style', sassLoaderLocal);
+    sassLoaderGlobal = ExtractTextPlugin.extract('style', sassLoaderGlobal);
   } else {
     cssLoaderLocal = 'style!' + cssLoaderLocal;
     cssLoaderGlobal = 'style!' + cssLoaderGlobal;
     lessLoaderLocal = 'style!' + lessLoaderLocal;
     lessLoaderGlobal = 'style!' + lessLoaderGlobal;
+    sassLoaderLocal = 'style!' + sassLoaderLocal;
+    sassLoaderGlobal = 'style!' + sassLoaderGlobal;
   }
   if (cssModules) {
     return [
@@ -35,6 +41,14 @@ export default ({ extractCss, cssModules }) => {
         test: /\.less$/,
         loader: lessLoaderGlobal,
         include: /node_modules/
+      }, {
+        test: /\.(sass|scss)$/,
+        loader: sassLoaderLocal,
+        exclude: /node_modules/
+      }, {
+        test: /\.(sass|scss)$/,
+        loader: sassLoaderGlobal,
+        include: /node_modules/
       }
     ];
   } else {
@@ -45,6 +59,9 @@ export default ({ extractCss, cssModules }) => {
       }, {
         test: /\.less$/,
         loader: lessLoaderGlobal
+      }, {
+        test: /\.(sass|scss)$/,
+        loader: sassLoaderGlobal
       }
     ]
   }
